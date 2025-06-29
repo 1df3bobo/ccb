c585d1f7460dbc61eefacdd00d8c52f4c71c8b38
DAX <272565923@qq.com>
2025-06-29 20:09:29 +0800

    去掉功能开发提示

--- ccb/lib/pages/tabs/wealth/children/lqb1/logic.dart
+++ ccb/lib/pages/tabs/wealth/children/lqb1/logic.dart
@@ -17,26 +17,21 @@ class Lqb1Logic extends GetxController {
 
   // 转入操作
   void onTransferIn() {
-    Get.snackbar('提示', '转入功能开发中');
   }
 
   // 分享操作
   void onShare() {
-    Get.snackbar('提示', '分享功能开发中');
   }
 
   // 更多操作
   void onMore() {
-    Get.snackbar('提示', '更多功能开发中');
   }
 
   // 产品详情
   void onProductDetail() {
-    Get.snackbar('提示', '产品详情功能开发中');
   }
 
   // 购买及问题
   void onBuyAndQuestion() {
-    Get.snackbar('提示', '购买及问题功能开发中');
   }
 } 
